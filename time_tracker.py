45f461641df56a41897a16e75edeef9963a234b6
Max Harris <max.o.harris@outlook.com>
2018-07-07 19:56:57 +0100

    added better scheduling

--- time_tracker.py
+++ time_tracker.py
@@ -1,5 +1,9 @@
 import datetime
 
+TWENTY_FOUR_HOUR_CLOCK_POST_HOUR = 17 
+POST_EVERY_DAYS = 1
+
+
 def get_current_time():
 	return datetime.datetime.now()
 
@@ -8,5 +12,5 @@ def get_current_time_plus_seconds(seconds):
 
 def get_sleep_time():
 	current_time = datetime.datetime.now()
-	time_of_next_post = datetime.datetime(*(datetime.datetime.today().timetuple()[:3])) + datetime.timedelta(days=1, hours=17)
+	time_of_next_post = datetime.datetime(*(datetime.datetime.today().timetuple()[:3])) + datetime.timedelta(days=POST_EVERY_DAYS, hours=TWENTY_FOUR_HOUR_CLOCK_POST_HOUR)
 	return (time_of_next_post-current_time).total_seconds()
